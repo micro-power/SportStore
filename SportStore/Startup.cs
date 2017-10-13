8159dbe376cc4d5e5b163321ea5aa057eeb0a906
firefield2 <wojtekgomola@gmail.com>
2017-10-13 16:16:37 +0200

    Add project files.

---
+++ SportStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportStore.Startup))]
+namespace SportStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
