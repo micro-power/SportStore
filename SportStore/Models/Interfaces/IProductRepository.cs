32e9cb279584666e6e9db35d3964227ac493cfde
firefield2 <wojtekgomola@gmail.com>
2017-10-13 16:48:46 +0200

    Added Product Repository Interface and DTO Product class.

---
+++ SportStore/Models/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SportStore.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportStore.Models.Interfaces
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
