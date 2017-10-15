f8b7f73f2f43d2900958bb01a54cacee27a755c5
firefield2 <wojtekgomola@gmail.com>
2017-10-15 19:32:29 +0200

    15/10/2017

---
+++ SportStore.Tests/PagingTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SportStore.HtmlHelpers;
+using SportStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SportStore.Tests
+{
+    [TestFixture]
+    class PagingTests
+    {
+        [Test]
+        public void Can_Generate_Page_Links()
+        {
+            // przygotowanie — definiowanie metody pomocniczej HTML — potrzebujemy tego,
+            // aby użyć metody rozszerzającej
+            HtmlHelper myHelper = null;
+            // przygotowanie — tworzenie danych PagingInfo
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+            // przygotowanie — konfigurowanie delegatu z użyciem wyrażenia lambda
+            Func<int, string> pageUrlDelegate = i => "Strona" + i;
+            // działanie
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+
+            // asercje
+            result.ToString().Should().Be(@"<a class=""btn btn-default"" href=""Strona1"">1</a>"
+            + @"<a class=""btn btn-default btn-primary selected"" href=""Strona2"">2</a>"
+            + @"<a class=""btn btn-default"" href=""Strona3"">3</a>");
+        }
+    }
+}
--- SportStore.Tests/ProductControllerTests.cs
+++ SportStore.Tests/ProductControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using NUnit.Framework;
 using SportStore.Controllers;
+using SportStore.Models;
 using SportStore.Models.Entities;
 using SportStore.Models.Interfaces;
 using System;
@@ -30,13 +31,36 @@ namespace SportStore.Tests
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             // działanie
-            IEnumerable<Product> result =
-            (IEnumerable<Product>)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
             // asercje
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+        [Test]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // przygotowanie
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+new Product {ProductID = 1, Name = "P1"},
+new Product {ProductID = 2, Name = "P2"},
+new Product {ProductID = 3, Name = "P3"},
+new Product {ProductID = 4, Name = "P4"},
+new Product {ProductID = 5, Name = "P5"}
+});
+            // przygotowanie
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            // działanie
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            // asercje
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 
